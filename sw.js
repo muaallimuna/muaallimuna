5e5d9912eb3f91e190559e0a803fe8cfb0d5acf1
Muaallimuna <muaallimuna@gmail.com>
2026-05-21 18:49:05 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'muallimuna-v1';
+const FILES = [
+  '/',
+  '/index.html',
+  '/muallimuna-tdah-kids.html',
+  '/muallimuna-alphabet-tdah.html',
+  '/muallimuna-lettres-difficiles.html',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
